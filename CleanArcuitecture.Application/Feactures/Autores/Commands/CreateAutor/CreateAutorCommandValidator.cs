3516c58a1452ed67d3f863c73699a702914786ad
deyner2002 <solano27deiner@gmail.com>
2022-03-18 00:33:28 -0500

    validaciones al crear autores

--- CleanArcuitecture.Application/Feactures/Autores/Commands/CreateAutor/CreateAutorCommandValidator.cs
+++ CleanArcuitecture.Application/Feactures/Autores/Commands/CreateAutor/CreateAutorCommandValidator.cs
@@ -6,8 +6,7 @@ namespace CleanArcuitecture.Application.Feactures.Autores.Commands
     {
         public CreateAutorCommandValidator()
         {
-
-            RuleFor(p => p.Nombre)
+              RuleFor(p => p.Nombre)
                 .NotEmpty().WithMessage("{Nombre} no puede estar vacio")
                 .NotNull()
                 .MaximumLength(50).WithMessage("{Nombre} no puede exceder los 50 caracteres");
