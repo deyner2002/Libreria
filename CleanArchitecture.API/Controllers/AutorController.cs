1f87c44a669ba34bee54c056be05bf726376e037
deyner2002 <solano27deiner@gmail.com>
2022-03-18 10:50:18 -0500

    Controllers para autor (post, put,delete) y libro (get)

---
+++ CleanArchitecture.API/Controllers/AutorController.cs
@@ -0,0 +1,55 @@
+﻿using CleanArcuitecture.Application.Feactures.Autores.Commands;
+using CleanArcuitecture.Application.Feactures.Autores.Commands.DeleteAutor;
+using CleanArcuitecture.Application.Feactures.Autores.Commands.UpdateAutor;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace CleanArchitecture.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class AutorController: ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AutorController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost(Name = "CreateAutor")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<ActionResult<int>> CreateAutor([FromBody] CreateAutorCommand createAutorCommand)
+        {
+          return await _mediator.Send(createAutorCommand);
+        }
+
+        [HttpPut( Name = "UpdateAutor")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> UpdateAutor([FromBody] UpdateAutorCommand updateAutorCommand)
+        {
+            await _mediator.Send(updateAutorCommand);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}", Name = "DeleteAutor")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> DeleteAutor(int id)
+        {
+            var command = new DeleteAutorCommand
+            {
+                Id = id
+            };
+
+           await _mediator.Send(command);
+
+            return NoContent();
+        }
+    }
+}
---
+++ CleanArchitecture.API/Controllers/LibroController.cs
@@ -0,0 +1,30 @@
+﻿using CleanArcuitecture.Application.Feactures.Libros.Queries.GetLibrosList;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace CleanArchitecture.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class LibroController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public LibroController(IMediator mediator)
+        {
+            this._mediator = mediator;
+        }
+
+        [HttpGet("{userName}", Name = "GetLibro")]
+        [ProducesResponseType(typeof(IEnumerable<LibrosVm>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<LibrosVm>>> GetLibrosByUserName(string username)
+        {
+            var query = new GetLibrosListQuery(username);
+            var libros = await _mediator.Send(query);
+            return Ok(libros);
+        }
+
+    }
+}
